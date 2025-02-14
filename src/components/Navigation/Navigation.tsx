e48412ad09e44b2ab7e9fafae3b15af612755038
Matthew Davoren <matthewdavoren@gmail.com>
2025-02-14 15:16:08 -0800

    new color pallete with dark grey for nav bar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaSearch, FaUserCircle } from "react-icons/fa";
+import Logo from "../../assets/J0013_DAIN_BRAMAGE_LOGO_V01.svg";
 import HomeIcon from "../../assets/DBwebsiteIconDBTV.svg";
 import RemoteIcon from "../../assets/DB_Remote.svg";
 import TvGuideIcon from "../../assets/DBwebsiteIconOpenSource.svg";
@@ -8,10 +9,19 @@ import "./Navigation.scss";
 const SearchNavBar: React.FC = () => {
   return (
     <div className="search-navbar">
-      <div className="search-navbar__search">
-        <FaSearch className="search-navbar__icon" size={24} />
-        <input type="text" placeholder="Search" className="search-navbar__input" />
+      <div className="search-navbar__left">
+        <a href="/">
+          <img src={Logo} alt="Dain Bramage TV" className="search-navbar__logo" />
+        </a>
       </div>
+
+      <div className="search-navbar__center">
+        <div className="search-navbar__search">
+          <FaSearch className="search-navbar__icon" size={24} />
+          <input type="text" placeholder="Search" className="search-navbar__input" />
+        </div>
+      </div>
+
       <div className="search-navbar__links">
         <a href="/" className="search-navbar__link">
           <img src={HomeIcon} alt="Home" />
@@ -25,6 +35,7 @@ const SearchNavBar: React.FC = () => {
         <FaUserCircle className="search-navbar__profile-icon" size={24} />
       </div>
     </div>
+
   );
 };
 
