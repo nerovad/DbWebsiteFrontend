6d9b8c4c81e871067a2b5d6cf98ae8531fd89124
Matthew Davoren <matthewdavoren@gmail.com>
2025-02-14 17:18:13 -0800

    initial build of channels sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/Navigation/Navigation.tsx";
 import NewsTicker from "./components/NewsTicker/NewsTicker.tsx";
-import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer.tsx";
+import Channels from "./components/Channels/Channels.tsx";
 
 const App: React.FC = () => {
   return (
     <div>
       <NavBar />
+      <Channels />
       <main className="p-4"> {/* Your main content goes here */}</main>
       <VideoPlayer />
       <NewsTicker />
---
+++ src/components/Channels/Channels.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Channels.scss";
+import LeftArrowIcon from "../../assets/Left_Arrow.svg";
+
+const Channels: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const channels = [
+    { name: "Channel 2: Dain Bramage", viewers: "27.4k watching" },
+    { name: "Channel 17: Music", viewers: "19.2k watching" },
+    { name: "Channel 29: Skateboarding", viewers: "18.5k watching" },
+    { name: "Channel 31: Horror", viewers: "17.6k watching" },
+    { name: "Channel 99: Film Fest", viewers: "17.4k watching" },
+    { name: "Channel 104: Late Nite Movies", viewers: "15.1k watching" },
+    { name: "Channel 108: Sports Bloopers", viewers: "10.9k watching" },
+    { name: "Channel 145: Home Movies", viewers: "9.7k watching" },
+    { name: "Channel 179: Public TV", viewers: "4.4k watching" },
+    { name: "Channel 199: Documentaries", viewers: "2.6k watching" },
+  ];
+
+  return (
+    <div className={`trending-container ${isOpen ? "open" : ""}`}>
+      <button className="toggle-button" onClick={toggleMenu}>
+        <img src={LeftArrowIcon} alt="Toggle" />
+      </button>
+      <div className={`trending-channels ${isOpen ? "open" : ""}`}>
+        <h3>Trending Channels</h3>
+        <ul>
+          {channels.map((channel, index) => (
+            <li key={index}>
+              {channel.name}
+              <h4>{channel.viewers}</h4>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Channels;
+
